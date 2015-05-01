9c3aee715509a5ca16a34d324aa41790fbfd1d8e
newuser <newuser@newusers-iMac.home>
2015-05-01 10:31:15 -0400

    Initial Commit

---
+++ BizApp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  BizApp
+//
+//  Created by newuser on 5/1/15.
+//  Copyright (c) 2015 Power Streamer LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
